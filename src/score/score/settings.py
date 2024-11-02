8081c785dc97b30c5bf8d0d1d37e96279dc88ac1
akirsuzuki <a.suzuki@vancreworth.com>
2024-11-02 11:51:00 +0900

    staticfiles in settings.py

--- src/score/score/settings.py
+++ src/score/score/settings.py
@@ -116,15 +116,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-
 STATIC_URL = 'static/'
 
+# Corrected STATICFILES_DIRS in score-ai/src/score/score/settings.py
 STATICFILES_DIRS = [
-    BASE_DIR / "/score/static",
-    # "/score/static/",
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'scoreai', 'static'),
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
