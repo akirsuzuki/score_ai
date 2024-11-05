68e0de05bb0b9e725af2e2495a5ed77e6a6034d8
akirsuzuki <a.suzuki@vancreworth.com>
2024-11-06 08:43:16 +0900

    modify asgi.py 'DJANGO_SETTINGS_MODULE'

--- src/score/score/asgi.py
+++ src/score/score/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'score.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.score.score.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'score.settings')
 
 application = get_asgi_application()
