13875eb4b7de282b8453943034067f8894cbb8e4
akirsuzuki <a.suzuki@vancreworth.com>
2024-11-06 19:51:05 +0900

    apps.pyを元に戻す&__init__.pyをsrcに追加

--- src/score/scoreai/apps.py
+++ src/score/scoreai/apps.py
@@ -3,6 +3,6 @@ from django.apps import AppConfig
 
 class ScoreaiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    # name = 'scoreai' # 元々はこれ
+    name = 'scoreai' # 元々はこれ
     # name = 'score.scoreai' # これだとDeployでエラー
-    name = 'src.score.scoreai'
+    # name = 'src.score.scoreai' # これだとDeployでエラー
