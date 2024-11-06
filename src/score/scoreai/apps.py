45b46ecb25b876836f346cc3e9a3c3967b3bdee5
akirsuzuki <a.suzuki@vancreworth.com>
2024-11-06 16:27:58 +0900

    modify apps.py to 'name = 'score.scoreai''

--- src/score/scoreai/apps.py
+++ src/score/scoreai/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class ScoreaiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'scoreai'
+    # name = 'scoreai'
+    name = 'score.scoreai'
