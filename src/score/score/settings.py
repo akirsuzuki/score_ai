d0d1f46516b8a5a676c08c6a7d0bec017cdd5bbc
akirsuzuki <a.suzuki@vancreworth.com>
2024-11-07 20:38:33 +0900

    add 'scoreai' in INSTALLED_APPS in settings.py

--- src/score/score/settings.py
+++ src/score/score/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
      # 以下追加
+    'scoreai',
     'rest_framework',
     'django_bootstrap5',
     'django.contrib.humanize',
